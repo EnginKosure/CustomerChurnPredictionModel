817c6af4c89ea4668e8603c5607aad6e56b2e7f1
EnginKosure <engin.kosure@cronos.be>
2021-01-22 12:56:33 +0100

    updata helper

--- data_dict.py
+++ data_dict.py
@@ -2,5 +2,7 @@ findings = {
     'a': 'AutoML and high-code model trials return almost the same output',
     'b': 'AutoML makes data-cleaning and feature selection implicitly',
     'c': 'Quality and relevance of the data is the main determinant',
-    'd': 'Consuming the model via web interface could provide a better UX'
+    'd': 'Consuming the model via web interface could provide a better UX',
+    'p': 'is likely to churn. You had better take some action!',
+    'pp': 'is happy and loyal. No need to take extra precautions.'
 }
